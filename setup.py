b5cce3bdf40c083957b789909719162cbf116a1d
liudong <lidong6@sugon.com>
2025-06-11 17:25:19 +0800

    修改setup.py 中依赖缺失问题

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension,find_packages
 
 # 默认'python setup.py install'只安装onescience
 # 依赖列表
-required_package = [
+install_requires = [
     'numpy>=1.25.0,<2.0.0',
     'xarray>=2023.1.0',
     'zarr>=2.14.2',
@@ -34,12 +34,10 @@ required_package = [
     'pandas>=2.2.2',
     'omegaconf>=2.3.0',
     'dask>=2024.11.1',
+    'seaborn',
+    'torchdata<=0.9.0',
 ]
 
-deps = {b: a for a, b in (re.findall(r"^(([^!=<>~ ]+)(?:[!=<>~ ].*)?$)", x)[0] for x in required_package)}
-
-def deps_list(*pkgs):
-    return [deps[pkg] for pkg in pkgs]
 
 def parse_requirements_file(filename):
     """Parse a requirements file into a list of dependencies."""
@@ -48,66 +46,25 @@ def parse_requirements_file(filename):
 
 extras = {}
 
-extras["jax"] = deps_list()
-extras["earth"] = deps_list()
-extras["cfd"] = deps_list()
-extras["alphafold"] = deps_list()
-#extras["protenix"] = parse_requirements_file("requirements-protenix.txt")
-# 默认情况只会搜索当前目录下各个含有__init.__.py的包，此字段
-# 可引入非python文件，如注释中所示
-package_data = {
-    # '': [
-    #     '*.so*',
-    #     '*.pyd',
-    #     'bin/*',
-    #     'lib/*.so*',
-    #     'lib/*.a',
-    #     'include/*'
-    #     'build_info.txt'
-    # ],
-    # '_c_minddata': ['lib_c_minddata*.so']
-}
+extras["jax"] = [
 
-install_requires = [
-    deps["numpy"],
-    deps["xarray"],
-    deps["nvtx"],
-    deps["timm"],
-    deps["pyyaml"],
-    deps["hydra-core"],
-    deps["scikit-learn"],
-    deps["zarr"],
-    deps["h5py"],
-    deps["s3fs"],
-    deps["pytz"],
-    deps["tqdm"],
-    deps["termcolor"],
-    deps["wandb"],
-    deps["mlflow"],
-    deps["vtk"],
-    deps["netcdf4"],
-    deps["pandas"],
-    deps["omegaconf"],
-    deps["treelib"],
-    deps["pyvista"],
-    deps["dask"]
 ]
+
 
 setup(
     name="onescience",
-    version="0.1.0",
+    version="0.1.0-rc1",
     author="sugon-ai4s",
     author_email="ai4s@sugon.com",
     description="First release",
     long_description="OneScience is a scientific computing toolkit built on an advanced deep learning framework",
-    url="http://10.0.54.20/dcutoolkit/hpcapps/onescience/onescience",
+    url="https://github.com/hpccube/OneScience",
     package_dir={"": "src"},
     packages=find_packages("src"),
     #packages=find_packages(include=["*science*"]),
-    package_data=package_data,
     extras_require=extras,
     include_package_data=True,
     install_requires=list(install_requires),
-    python_requires=">=3.8.0",
+    python_requires=">=3.10.0",
     zip_safe=False,
 )
