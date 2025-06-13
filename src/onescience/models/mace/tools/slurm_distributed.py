114a91393ad9057f9b27d7b2e5c0b5ac590cec6c
liudong <lidong6@sugon.com>
2025-06-13 09:44:12 +0800

    fix mace distr env

--- src/onescience/models/mace/tools/slurm_distributed.py
+++ src/onescience/models/mace/tools/slurm_distributed.py
@@ -12,7 +12,7 @@ import hostlist
 
 class DistributedEnvironment:
     def __init__(self):
-        self._setup_distr_env()
+        # self._setup_distr_env()
         self.master_addr = os.environ["MASTER_ADDR"]
         self.master_port = os.environ["MASTER_PORT"]
         self.world_size = int(os.environ["WORLD_SIZE"])
